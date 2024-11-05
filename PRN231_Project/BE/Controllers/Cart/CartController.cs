0262c7734b60a2f19b425f21d1bdcc6edc8e2047
tuannm <Minhtuan_81>
2024-11-06 00:14:58 +0700

    update

--- PRN231_Project/BE/Controllers/Cart/CartController.cs
+++ PRN231_Project/BE/Controllers/Cart/CartController.cs
@@ -5,6 +5,7 @@ using Lib.DTO.Cart;
 using Lib.Models;
 using Lib.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace BE.Controllers.Cart;
 
@@ -18,7 +19,8 @@ public class CartController : ControllerBase
     private readonly IVnPayService _vnPayService;
 
 
-    public CartController(IVnPayService vnPayService,IRepository<Lib.Models.Bill> billRepository,IRepository<Course> courseRepository, IRepository<BillDetail> billDetailRepository)
+    public CartController(IVnPayService vnPayService, IRepository<Lib.Models.Bill> billRepository,
+        IRepository<Course> courseRepository, IRepository<BillDetail> billDetailRepository)
     {
         _vnPayService = vnPayService;
         _courseRepository = courseRepository;
@@ -27,7 +29,7 @@ public class CartController : ControllerBase
     }
 
     public Dictionary<int, CartDTO> cartList;
-    
+
     [HttpPost("CreatePaymentUrl")]
     public async Task<IActionResult> CreatePaymentUrl(PaymentInformationModel model)
     {
@@ -37,37 +39,67 @@ public class CartController : ControllerBase
     }
 
     [HttpPost("FinishPayment")]
-    public async Task<IActionResult> FinishPayment()
+    public async Task<IActionResult> FinishPayment([FromBody] PaymentRequestDTO paymentRequest)
     {
-        var response = _vnPayService.PaymentExecute(Request.Query);
+        var queryDictionary = new Dictionary<string, StringValues>
+        {
+            { "vnp_Amount", paymentRequest.vnp_Amount.ToString() },
+            { "vnp_BankCode", paymentRequest.vnp_BankCode },
+            { "vnp_BankTranNo", paymentRequest.vnp_BankTranNo },
+            { "vnp_CardType", paymentRequest.vnp_CardType },
+            { "vnp_OrderInfo", paymentRequest.vnp_OrderInfo },
+            { "vnp_PayDate", paymentRequest.vnp_PayDate },
+            { "vnp_ResponseCode", paymentRequest.vnp_ResponseCode },
+            { "vnp_TmnCode", paymentRequest.vnp_TmnCode },
+            { "vnp_TransactionNo", paymentRequest.vnp_TransactionNo },
+            { "vnp_TransactionStatus", paymentRequest.vnp_TransactionStatus },
+            { "vnp_TxnRef", paymentRequest.vnp_TxnRef },
+            { "vnp_SecureHash", paymentRequest.vnp_SecureHash }
+        };
+
+        var rq = new QueryCollection(queryDictionary);
+
+
+        // Execute the payment and capture the response using paymentRequest properties
+        var response = _vnPayService.PaymentExecute(rq); // Adjust this method accordingly
+
+        // Retrieve the cart list from cookies
         var cartListJson = Request.Cookies["cartList"];
-        if (response.Success == true)
+        if (response.Success)
         {
             if (!string.IsNullOrEmpty(cartListJson))
             {
-                cartList = JsonSerializer.Deserialize<Dictionary<int, CartDTO>>(cartListJson);
-                var totalPayment = cartList.Sum(x=>x.Value.Course.Price);
-                _billRepository.AddAsync(new Lib.Models.Bill
+                // Deserialize the cart list from JSON
+                var cartList = JsonSerializer.Deserialize<Dictionary<int, CartDTO>>(cartListJson);
+                var totalPayment = cartList.Sum(x => x.Value.Course.Price);
+
+                // Create and save the bill
+                var bill = new Lib.Models.Bill
                 {
                     TotalPayment = totalPayment,
-                    UserId = 1
-                });
-                var billID = _billRepository.GetLastAsync(x => x.Id).Id;
+                    UserId = 1 // Consider replacing with the actual user ID
+                };
+                await _billRepository.AddAsync(bill);
+
+                // Get the last inserted bill ID
+                var billID = (await _billRepository.GetLastAsync(x => x.Id)).Id;
+
+                // Add bill details for each course in the cart
                 foreach (var item in cartList)
                 {
-                    _billDetailRepository.AddAsync(new BillDetail
+                    var billDetail = new BillDetail
                     {
                         BillId = billID,
                         CourseId = item.Value.Course.Id
-                    });
+                    };
+                    await _billDetailRepository.AddAsync(billDetail);
                 }
-            } 
+            }
+
             return Ok("Finish Payment Successfully");
         }
-       
 
-        return BadRequest();
+        // Handle case where payment was not successful
+        return BadRequest("Payment failed or invalid cart data.");
     }
-    
 }
-
